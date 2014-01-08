8a71116bd8ae4c416a723b826ac273e490c9dbe9
Renaud <renaud@tx.technion.ac.il>
2014-01-08 11:41:17 +0200

    Initial import

---
+++ R/SQLquery.R
@@ -0,0 +1,193 @@
+# Project: dbutils
+# 
+# Author: Renaud Gaujoux
+# Created: Jan 8, 2014
+###############################################################################
+
+#' SQL Special Types
+#' 
+#' These variables can be used in SQL statements defined by \code{\link{SQLquery}} objects. 
+#' 
+#' @rdname SQLtypes
+#' @export
+sql_NULL <- as.expression('NULL')
+#' @rdname SQLtypes
+#' @export
+sql_NOW <- as.expression('NOW()')
+
+#' Insert Single Record into an SQL Table
+#' 
+#' @param con SQL database connection object
+#' @param ... named arguments used as pairs (field, value)
+#' @param .table name of the table into which the recored is inserted
+#' @param .verbose verbose level
+#' 
+#' @return the primary key of the newly inserted record
+#' 
+#' @examples
+#' 
+#' # connect to sample database
+#' con <- .dbSampleConnection()
+#' dbGetQuery(con, 'SELECT * FROM test')
+#' 
+#' # insert single record 
+#' id <- dbINSERT(con, a = 1, b = 'blabla', c = NA, t = sql_NOW, .table = 'test')
+#' id
+#' 
+#' # query table again
+#' dbGetQuery(con, 'SELECT * FROM test')
+#' 
+#' # clean up
+#' dbEMPTY(con, 'test')
+#' 
+#' @export
+dbINSERT <- function(con, ..., .table, .verbose = FALSE){
+	
+    table_name <- .table
+    sql <- SQLquery(table_name)
+    sql$add(...)
+    ins <- sql$insert_query()
+    if( .verbose ) cat("\n", ins, "\n", sep='')
+	rs <- dbSendQuery(con, ins)
+	dbClearResult(rs)
+	invisible(dbGetQuery(con, "SELECT LAST_INSERT_ID()")[1,1])
+}
+
+#' Insert Multiple Records into an SQL Table
+#' 
+#' Inserts multiple data records into an SQL table directly from a \code{data.frame}.
+#' 
+#' @inheritParams dbINSERT
+#' @param x \code{data.frame} object, whose column names correspond to field names, and 
+#' rows to the records to be inserted.
+#' @param .dump logical or character vector that indicates if the insertion query should be 
+#' only dumped into a file.  
+#' 
+#' @export
+#' @examples 
+#' # connect to sample database
+#' con <- .dbSampleConnection()
+#' dbGetQuery(con, 'SELECT * FROM test')
+#' 
+#' # insert single record
+#' df <- data.frame(a = 1:3, b = letters[1:3], c = c(1.5, NA, 6.5)) 
+#' id <- dbINSERTx(con, df, .table = 'test')
+#' id
+#' 
+#' # query table again
+#' dbGetQuery(con, 'SELECT * FROM test')
+#' 
+#' # clean up
+#' dbEMPTY(con, 'test')
+#' 
+dbINSERTx <- function(con, x, .table, .verbose = FALSE, .dump = FALSE){
+    
+    dump <- .dump
+    table_name <- .table
+    sql <- SQLquery(table_name)
+    qt <- system.time(query <- sql$minsert_query(mdata = x))
+    # print(qt)
+    if( isFALSE(dump) ){ # direct query
+        
+        rs <- dbSendQuery(con, query)
+        dbClearResult(rs)
+        invisible(dbGetQuery(con, "SELECT LAST_INSERT_ID()")[1,1])
+    }else{ # dump into file and import
+        tmp <- if( is.character(dump) ) dump 
+               else{
+                   tempfile(fileext = ".sql")
+               }
+        ft <- system.time(cat(query, file = tmp))
+        invisible(tmp)
+    }
+}
+
+SQLquery <- function(table_name){
+    
+    .fields <- list()
+    .mdata <- list()
+    .table_name <- table_name
+    .stick <- function(fields, sep = if( value.only ) ',' else '=', value.only = FALSE){
+        ins <- sapply(names(fields), function(f){
+                    v <- fields[[f]]
+                    qv <- if( is.character(v) ) paste0('"', v, '"') 
+                            else if( length(v) == 1L && (is.null(v) || is_NA(v)) ) sql_NULL
+                            else as.character(v)
+                    if( value.only ) qv
+                    else paste0("`", f, "` ", sep, ' ', qv)
+                }, simplify = !is.list(fields) || length(fields[[1]]) == 1L)
+        
+        if( value.only ){
+            names(ins) <- NULL
+            ins$sep = sep
+            do.call(paste, ins)
+        }else ins
+    }
+    
+    
+    .update_query <- function(id, table_name = .table_name, idfield = 'id'){
+        sql <- .stick(.fields)
+        sql <- paste0(sql, collapse = ", ")
+        paste0('UPDATE `', table_name, "` SET ", sql
+                , " WHERE `", idfield, "` = '", id, "';"
+                , collapse = "\n")
+    }
+    
+    .add_field <- function(name, value, escape = FALSE){
+        
+        # escape special characters if necessary
+        if( escape ) value <- escape_sql(value)
+        # add to field list
+        .fields[[name]] <<- value 
+    }
+    
+    .add <- function(..., escape = FALSE){
+        fields <- list(...)
+        if( length(fields) == 1L && is.null(names(fields)) )
+            fields <- as.list(fields[[1]])
+        lapply(seq_along(fields), function(i){
+                    .add_field(names(fields)[i], fields[[i]], escape = escape)
+                })
+        length(fields)
+    }
+    
+    list(
+            size = function() length(.fields)
+            , set_fields = function(...){
+                fields <- list(...)
+                if( length(fields) == 1L && is.null(names(fields)) )
+                    fields <- as.list(fields[[1]])
+                .fields <<- fields
+            }
+            , add = .add
+            , add_field = .add_field
+            , update_query = .update_query 
+            , sendUpdate = function(con, ...){
+                sql <- .update_query(...)
+                rs <- dbSendQuery(con, sql)
+                dbClearResult(rs)
+            }
+            , madd = function(...){
+                if( length(.fields) ){
+                    .mdata <<- c(.mdata, list(.fields))
+                    .fields <<- list()
+                }
+                .add(...)
+                .mdata <<- c(.mdata, list(.fields))
+                .fields <<- list()
+            }
+            , minsert_query = function(table_name = .table_name, mdata = .mdata){
+                sql <- .stick(mdata, value.only = TRUE)
+                paste0("INSERT INTO `", table_name, "` ("
+                        , paste0('`', names(mdata), '`', collapse = ",")
+                        , ") VALUES ", paste0("(", sql, ")", collapse = ","))
+            }
+            , insert_query = function(table_name = .table_name){
+                sql <- .stick(.fields)
+                sql <- paste0(sql, collapse = ", ")
+                sql <- paste0('INSERT INTO `', table_name, "` SET ", sql, ";"
+                        , collapse = "\n")
+            }
+    )
+}
+
---
+++ R/dbManager.R
@@ -0,0 +1,106 @@
+# Project: dbutils
+# 
+# Author: Renaud Gaujoux
+# Created: Jan 8, 2014
+###############################################################################
+
+
+#' Database Connection Manager
+#' 
+#' Connects to a database, possibly after multiple tries, 
+#' e.g., if the number of concurrent connections hit its limit.
+#' 
+#' This function is a wrapper around \code{\link{dbConnect}}, 
+#' which provides the following extra features:
+#' 
+#' \describe{
+#' \item{Re-try with timeout}{useful when running many batch array jobs,
+#' which query a database that have a limited number of concurrent connection.}
+#' \item{Caching}{the last opened connection is cached and returned, if still valid}
+#' }
+#' 
+#' @param ... database connection specification(s)
+#' @param max maximum number of connection attempts (integer)
+#' @param timeout time (in seconds) after which an error is thrown if the connection could not be 
+#' opened.
+#' Within this period of time, \code{max} attempts are made, i.e. one every \code{timeout/max} seconds.
+#' @param cache logical that indicates if the last open connection should be returned if still valid. 
+#' @param verbose verbosity level
+#' @param where namespace of the calling package (not to be used)
+#' 
+#' @import DBI
+#' @importFrom utils packageName
+dbManager <- local({
+            
+        # cached connection
+        .db <- NULL
+        function(..., max = 20, timeout = 10, cache = TRUE, verbose = FALSE, where = topenv(parent.frame())){
+            
+                # early exit if not a string
+                if( !isString(dbname) ) return(dbname)
+                # check previous connection
+                if( cache && !is.null(.db) ){
+                    # return cached connection object if still valid
+                    if( dbIsValid(.db) ) return(.db)
+                }
+                # reset old connection object (do not close the connection)
+                .db <<- NULL
+                
+                # detect driver
+                if( grepl("^([^:]+):", dbname) ){
+                    drvName <- gsub("^([^:]+):.*", "\\1", dbname)
+                    dbname <- gsub("^([^:]+):", "", dbname)
+                }else{
+                    drvName <- 'SQLite'
+                    if( grepl("^@", dbname) ){# use db in package
+                        dbname <- system.file('db', paste0(gsub("^@", '', dbname), '.sqlite3'), package = packageName(where))
+                    }
+                }
+                library(pkg <- paste0('R', drvName), character.only = TRUE)
+                drv <- getFunction(drvName, where = paste0('package:', pkg))
+                
+                if( verbose ) message("Opening new connection to ", drvName, ":", dbname, ' ... ', appendLF = FALSE)
+                # try-connection loop
+                .try <- 1
+                while( .try <= max ){
+                        
+                    con <- tryCatch({
+                            if( drvName == 'MySQL' ){
+                                    dbConnect(drv(), group = dbname)
+                            }else dbConnect(drv(), dbname)
+                        }
+                        , error = function(e){
+                            if( .try >= max ){
+                                    stop("Timeout after ", max, " attempts of connection to ", drvName,":", dbname, " - ", e)
+                            }
+                            Sys.sleep( timeout / max )
+                        }) 
+            
+                    if( !is.null(con) ) break;
+                    .try <- .try + 1
+                }
+                if( verbose ) message('OK [', .try, ']')
+                
+                # cache and return connection object 
+                .db <<- con
+                con
+        }
+})
+
+# Tests parallel access to a MySQL database
+# To really see if the retry/timeout approach works,
+# one first needs to set the system variable to something low
+# max-connections = 10
+testCon <- function(){
+    
+    library(parallel)
+    res <- mclapply(1:20, function(i, ...){
+                    message("Thread ", i)
+                    db <- dbManager('MySQL:testdb')
+                    Sys.sleep(1)
+                    dbDisconnect(db)
+                    
+            }, mc.cores = 20)
+    
+}
+
